03d9a7338512b7f4a0728a664af3fc7dafa5b30d
michaela <civilmotha@gmail.com>
2025-10-14 18:20:25 +0200

    edit

--- p/p/settings.py
+++ p/p/settings.py
@@ -51,6 +51,7 @@ AUTHENTICATION_BACKENDS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,6 +166,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR.parent, "timesheets", "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
